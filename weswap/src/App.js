69c79898257fc8aa81546f2f5b0fa77b1ccd0e7e
Vishal Varma <55092724+vishalvarma07@users.noreply.github.com>
2023-02-11 16:56:00 -0600

    Basic Login
    login and routes

--- weswap/src/App.js
+++ weswap/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+ } from "react-router-dom"; 
+import {Login} from './components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "gradient-bg">
+        <Router>
+          <Routes>
+            <Route path ='/' element = {
+              <div>
+                <Login/>
+              </div>
+            } />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
